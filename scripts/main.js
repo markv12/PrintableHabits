337be8f98d0abc5c011783665646753dff33c96d
Scott <swsteffes@gmail.com>
2020-05-23 15:00:38 +0900

    fix up outside rectangle

--- scripts/main.js
+++ scripts/main.js
@@ -33,19 +33,26 @@ function Generate(){
 
 function GenerateSVG(voronoi_diagram, width, height){
   const svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg1.setAttribute("width", width);
-  svg1.setAttribute("height", height);
+  svg1.setAttribute("width", width+6);
+  svg1.setAttribute("height", height+6);
 
   for (var i = 0; i < voronoi_diagram.edges.length; i++) {
     var edge = voronoi_diagram.edges[i];
     var svg_line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    svg_line.setAttribute("x1", edge.va.x);
-    svg_line.setAttribute("y1", edge.va.y);
-    svg_line.setAttribute("x2", edge.vb.x);
-    svg_line.setAttribute("y2", edge.vb.y);
+    svg_line.setAttribute("x1", edge.va.x+3);
+    svg_line.setAttribute("y1", edge.va.y+3);
+    svg_line.setAttribute("x2", edge.vb.x+3);
+    svg_line.setAttribute("y2", edge.vb.y+3);
     svg_line.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:5");
-  
     svg1.appendChild(svg_line);
   }
+  var borderRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  borderRect.setAttribute("x", 2);
+  borderRect.setAttribute("y", 2);
+  borderRect.setAttribute("width", width+1);
+  borderRect.setAttribute("height", height+1);
+  borderRect.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:5;fill-opacity:0;");
+  svg1.appendChild(borderRect);
+
   return svg1;
 }
